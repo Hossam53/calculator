042a85c5d04efa8b94a3d1fcbb19bc57d8154eed
hossambabari <hossam.babari21@gmail.com>
2023-11-22 13:40:02 +0000

    add test2 testPlusZero() for RevPolishCalc and added a setUp method to avoid code repetiveness

--- Calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ Calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -2,10 +2,21 @@ package uk.ac.rhul.cs2800;
 
 public class RevPolishCalc {
 
-  public float evaluate(String s) {
-        return Float.parseFloat(s);
-      }
+  public float evaluate(String expr) {
+    String[] parts = expr.split(" ");
+    float operand1 = Float.parseFloat(parts[0]);
+    float operand2 = Float.parseFloat(parts[1]);
+    char operator = parts[2].charAt(0);
+
+    switch (operator) {
+      case '+':
+        return operand1 + operand2;
+      default:
+        return 0;
+    }
+  }
 }
+
 
 
 
--- Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
+++ Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
@@ -2,13 +2,22 @@ package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RevPolishCalcTest {
+  private  RevPolishCalc calc;
+  @BeforeEach
+  void setUp(){
+     calc = new RevPolishCalc();
+  }
   @Test
   void testSingleNumberEvaluation() {
-    RevPolishCalc calc = new RevPolishCalc();
     assertEquals(5, calc.evaluate("5"), "A single number should return itself.");
+  }
+  @Test
+  public void testPlusZero() {
+    assertEquals(5, calc.evaluate("5 0 +"), "Adding zero to a number should return the number itself.");
   }
 
 
