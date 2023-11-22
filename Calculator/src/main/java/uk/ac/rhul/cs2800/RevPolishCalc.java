986b8273ef15fc1750cc7d8509a1002d3fc5a185
hossambabari <hossam.babari21@gmail.com>
2023-11-22 14:51:12 +0000

    add test3 testPlusOne() implemented Stack operations in the RevPolishCalc class

--- Calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ Calculator/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,22 +1,61 @@
 package uk.ac.rhul.cs2800;
 
+import application.InvalidExpression;
+
 public class RevPolishCalc {
 
-  public float evaluate(String expr) {
-    String[] parts = expr.split(" ");
-    float operand1 = Float.parseFloat(parts[0]);
-    float operand2 = Float.parseFloat(parts[1]);
-    char operator = parts[2].charAt(0);
+  private Stack numStack;
 
-    switch (operator) {
-      case '+':
-        return operand1 + operand2;
-      default:
-        return 0;
+  public RevPolishCalc() {
+    numStack = new Stack();
+  }
+
+  public float evaluate(String expr) throws InvalidExpression {
+    try {
+      String[] tokens = expr.split("\\s+");
+      for (String token : tokens) {
+        if (token.matches("-?\\d+(\\.\\d+)?")) { // If it's a number
+          numStack.push(new Entry(Float.parseFloat(token)));
+        } else { // It's an operator
+          if (numStack.size() < 2) {
+            throw new InvalidExpression("Not enough operands for '" + token + "' operation.");
+          }
+          Entry operand2 = numStack.pop();
+          Entry operand1 = numStack.pop();
+
+          // Ensure both operands are numbers before performing operations
+          if (operand1.getType() != Type.NUMBER || operand2.getType() != Type.NUMBER) {
+            throw new InvalidExpression("Both operands must be numbers for the operation.");
+          }
+
+          float a = operand1.getValue();
+          float b = operand2.getValue();
+          float result;
+
+          switch (token) {
+            case "+":
+              result = a + b;
+              numStack.push(new Entry(result));
+              break;
+            // Implement other cases for different operations
+            default:
+              throw new InvalidExpression("Unsupported operation: " + token);
+          }
+        }
+      }
+      if (numStack.size() != 1) {
+        throw new InvalidExpression("Invalid RPN expression. Stack should have only one item left.");
+      }
+
+      // Ensure the result is a number before returning it
+      Entry resultEntry = numStack.pop();
+      if (resultEntry.getType() != Type.NUMBER) {
+        throw new InvalidExpression("Result should be a number.");
+      }
+
+      return resultEntry.getValue();
+    } catch (EmptyStack | BadType e) {
+      throw new InvalidExpression("Invalid expression: " + e.getMessage());
     }
   }
 }
-
-
-
-
--- Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
+++ Calculator/src/test/java/uk/ac/rhul/cs2800/RevPolishCalcTest.java
@@ -2,6 +2,7 @@ package uk.ac.rhul.cs2800;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import application.InvalidExpression;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +13,16 @@ class RevPolishCalcTest {
      calc = new RevPolishCalc();
   }
   @Test
-  void testSingleNumberEvaluation() {
+  void testSingleNumberEvaluation() throws  InvalidExpression {
     assertEquals(5, calc.evaluate("5"), "A single number should return itself.");
   }
   @Test
-  public void testPlusZero() {
+  public void testPlusZero() throws  InvalidExpression {
     assertEquals(5, calc.evaluate("5 0 +"), "Adding zero to a number should return the number itself.");
+  }
+  @Test
+  public void testPlusOne() throws  InvalidExpression {
+    assertEquals(6, calc.evaluate("5 1 +"), "Adding one to a number should increment the number.");
   }
 
 
